7a3de885217f4d0da5fd95441a4887f64f6faeb2
ai-strings <adrstrings@gmail.com>
2021-11-19 15:11:56 +0300

    Project Start

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,11 @@ export default {
 </script>
 
 <style>
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
