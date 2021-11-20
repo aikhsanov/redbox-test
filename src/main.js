e69a07430d24d121c0c4583fc31c735513dcf00d
ai-strings <adrstrings@gmail.com>
2021-11-20 14:35:16 +0300

    Added date picker

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import "@/css/tailwind.css";
 import Maska from "maska";
+import '@themesberg/flowbite';
 import App from "./App.vue";
 
 createApp(App).use(Maska).mount("#app");
